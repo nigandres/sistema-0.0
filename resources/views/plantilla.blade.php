1c0bf3d401c59dbb7203ea92edb75887ffa018bf
Unknown <andressolis1@hotmail.es>
2018-11-08 17:42:16 -0600

    cambio temporales

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+@yield('head')
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,7 +30,6 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
